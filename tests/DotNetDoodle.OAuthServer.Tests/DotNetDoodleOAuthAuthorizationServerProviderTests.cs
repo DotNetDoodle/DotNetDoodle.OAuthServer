6167df08e405d56d991c9cd5bba2ef27e1aacca2
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-03-04 20:05:21 +0200

    added ShouldSetTheClientToTheContextSuccessfully for DotNetDoodleOAuthAuthorizationServerProviderTests

--- tests/DotNetDoodle.OAuthServer.Tests/DotNetDoodleOAuthAuthorizationServerProviderTests.cs
+++ tests/DotNetDoodle.OAuthServer.Tests/DotNetDoodleOAuthAuthorizationServerProviderTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DotNetDoodle.OAuthServer.Infrastructure.Managers;
+using DotNetDoodle.OAuthServer.Infrastructure.Objects;
+using DotNetDoodle.OAuthServer.Infrastructure.Providers;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +14,23 @@ namespace DotNetDoodle.OAuthServer.Tests
     public class DotNetDoodleOAuthAuthorizationServerProviderTests
     {
         [Fact]
-        public async Task ShouldDo()
+        public async Task ShouldSetTheClientToTheContextSuccessfully()
         {
+            Mock<IClientManager> clientManagerMock = new Mock<IClientManager>();
+            clientManagerMock.Setup(c => c.GetVerfiedClientAsync("1234", "12345678"))
+                .Returns<string, string>((clientId, secret) => Task.FromResult(new Client 
+                {
+                    Id = clientId,
+                    Flow = Infrastructure.OAuthFlow.Client,
+                    RequireConsent = true,
+                    AllowRefreshToken = true
+                }));
+
+            Mock<IServiceProvider> serviceProvider = new Mock<IServiceProvider>();
+            serviceProvider.Setup(m => m.GetService(typeof(IClientManager))).Returns(clientManagerMock.Object);
+
+            var provider = new DotNetDoodleOAuthAuthorizationServerProvider(null, null);
+            provider.ValidateClientAuthentication(null);
         }
     }
 }

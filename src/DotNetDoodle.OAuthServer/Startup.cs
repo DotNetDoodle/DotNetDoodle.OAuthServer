1f2b7dd3610b28a8c3652312312d4bd38a89f806
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-03-04 13:09:13 +0200

    updated the dependencies

---
+++ src/DotNetDoodle.OAuthServer/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetDoodle.Owin.Dependencies.Adapters;
+
+namespace DotNetDoodle.OAuthServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}

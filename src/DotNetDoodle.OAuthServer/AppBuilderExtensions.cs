d2b6dde7b7906bb4de08eba305976bc676adbae6
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-03-04 15:10:28 +0200

    added extension methods for IAppBuilder to stand up the owin pipeline. Also, tweaked the Startup class to call those methods.

---
+++ src/DotNetDoodle.OAuthServer/AppBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using DotNetDoodle.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System.Web.Http;
+using WebApiContrib.Formatting.Razor;
+
+namespace DotNetDoodle.OAuthServer
+{
+    public static class AppBuilderExtensions
+    {
+        public static IAppBuilder RunWebApi(this IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Configure formatters
+            config.Formatters.Clear();
+            config.Formatters.Add(new RazorViewFormatter());
+
+            // Configure routes
+            config.Routes.MapHttpRoute("OAuthAuthorizeRoute", "oauth/authorize");
+
+            return app.UseWebApiWithContainer(config);
+        }
+
+        public static IAppBuilder RunOAuthServer(this IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions oAuthServerOptions = new OAuthAuthorizationServerOptions 
+            {
+            };
+
+            return app.UseOAuthAuthorizationServer(oAuthServerOptions);
+        }
+    }
+}
--- src/DotNetDoodle.OAuthServer/Startup.cs
+++ src/DotNetDoodle.OAuthServer/Startup.cs
@@ -3,7 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using DotNetDoodle.Owin.Dependencies.Adapters;
+using DotNetDoodle.Owin.Dependencies;
+using DotNetDoodle.Owin.Dependencies.Autofac;
+using Microsoft.Owin.Security.OAuth;
+using DotNetDoodle.Owin;
+using Autofac;
+using Microsoft.Owin.Logging;
 
 namespace DotNetDoodle.OAuthServer
 {
@@ -11,6 +16,32 @@ namespace DotNetDoodle.OAuthServer
     {
         public void Configuration(IAppBuilder app)
         {
+            ILogger logger = app.CreateLogger<Startup>();
+            logger.WriteInformation("OAuth Server is starting up.");
+
+            try
+            {
+                IContainer container = AutofacConfig.InitializeContainer();
+                app.UseAutofacContainer(container)
+                    .RunOAuthServer()
+                    .RunWebApi();
+            }
+            catch (Exception ex)
+            {
+                logger.WriteError("OAuth Server could not be registered correctly.", ex);
+                throw;
+            }
+
+            logger.WriteInformation("OAuth Server is successfully configured.");
+        }
+    }
+
+    public static class AutofacConfig
+    {
+        public static IContainer InitializeContainer()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            return builder.Build();
         }
     }
 }

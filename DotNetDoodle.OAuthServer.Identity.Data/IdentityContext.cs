52edd1223f704e174e1441981b92df7ee87d9e3c
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-03-07 16:38:27 +0200

    initial look of the DotNetDoodle.OAuthServer.Identity.Data is done

---
+++ DotNetDoodle.OAuthServer.Identity.Data/IdentityContext.cs
@@ -0,0 +1,9 @@
+﻿using DotNetDoodle.OAuthServer.Identity.Data.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DotNetDoodle.OAuthServer.Identity.Data
+{
+    public class IdentityContext : IdentityDbContext<User>
+    {
+    }
+}

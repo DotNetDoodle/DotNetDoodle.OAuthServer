0ee9f0c579df07d23e3009de13701acbf863a172
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-03-25 19:22:25 +0200

    added DotNetDoodle.OAuth.Domain proj

---
+++ src/DotNetDoodle.OAuth.Domain/Entities/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDoodle.OAuth.Domain.Entities
+{
+    public class Client
+    {
+        public Client(string name)
+        {
+            Id = Guid.NewGuid().ToString("N");
+        }
+
+        public string Id { get; protected set; }
+        public string Name { get; protected set; }
+    }
+}

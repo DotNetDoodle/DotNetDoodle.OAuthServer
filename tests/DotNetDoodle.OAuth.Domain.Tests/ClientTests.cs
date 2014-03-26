b46e08d632b4527334c35160f4f81e5bd5926606
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-03-26 12:01:52 +0200

    added DotNetDoodle.OAuth.Domain.Tests proj

---
+++ tests/DotNetDoodle.OAuth.Domain.Tests/ClientTests.cs
@@ -0,0 +1,19 @@
+﻿using DotNetDoodle.OAuth.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotNetDoodle.OAuth.Domain.Tests
+{
+    public class ClientTests
+    {
+        [Fact]
+        public void Client_Should_Throw_Null_Reference_Exception_If_The_Name_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Client(null, OAuthFlow.Client));
+        }
+    }
+}
